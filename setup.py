8416a20204d436b88500dc1119df37b03750b1fa
Hiroshiba Kazuyuki <hihokaruta@gmail.com>
2020-06-06 01:19:05 +0900

    remove singing note

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_namespace_packages
 # console scripts
 console_scripts = []
 
-extractors = list(Path('extractor').glob('extract_*'))
+extractors = list(Path("extractor").glob("extract_*"))
 assert len(extractors) > 0
 console_scripts += [
     f'{path.stem.replace("extract_", "acoustic_feature_extract_")}=extractor.{path.stem}:main'
     for path in extractors
 ]
 
-analyzers = list(Path('analyzer').glob('analyze_*'))
+analyzers = list(Path("analyzer").glob("analyze_*"))
 assert len(analyzers) > 0
 console_scripts += [
     f'{path.stem.replace("analyze_", "acoustic_feature_analyze_")}=analyzer.{path.stem}:main'
@@ -21,21 +21,13 @@ console_scripts += [
 
 # setup
 setup(
-    name='acoustic_feature_extractor',
-    version='0.0.1',
+    name="acoustic_feature_extractor",
+    version="0.0.1",
     packages=find_namespace_packages(),
-    url='https://github.com/Hiroshiba/acoustic_feature_extractor',
-    author='Kazuyuki Hiroshiba',
-    author_email='hihokaruta@gmail.com',
-    license='MIT License',
+    url="https://github.com/Hiroshiba/acoustic_feature_extractor",
+    author="Kazuyuki Hiroshiba",
+    author_email="hihokaruta@gmail.com",
+    license="MIT License",
     entry_points=dict(console_scripts=console_scripts),
-    install_requires=[
-        'numpy',
-        'scipy',
-        'librosa<0.8.0',
-        'pyworld',
-        'pysptk',
-        'tqdm',
-        'kiritan_singing_label_reader @ git+https://github.com/Hiroshiba/kiritan_singing_label_reader',
-    ],
+    install_requires=["numpy", "scipy", "librosa<0.8.0", "pyworld", "pysptk", "tqdm"],
 )
--- tests/test_import.py
+++ tests/test_import.py
@@ -4,52 +4,60 @@ import unittest
 class TestImport(unittest.TestCase):
     def test_import_analyzer(self):
         from analyzer import analyze_wave
+
         dir(analyze_wave)
 
     def test_import_extractor(self):
         from extractor import extract_collected_local
+
         dir(extract_collected_local)
 
         from extractor import extract_f0
+
         dir(extract_f0)
 
         from extractor import extract_melspectrogram
+
         dir(extract_melspectrogram)
 
-        from extractor import extract_midi_note
-        dir(extract_midi_note)
-
         from extractor import extract_phoneme
+
         dir(extract_phoneme)
 
         from extractor import extract_silence
+
         dir(extract_silence)
 
         from extractor import extract_volume
+
         dir(extract_volume)
 
         from extractor import extract_wave
+
         dir(extract_wave)
 
     def test_import_data(self):
         from acoustic_feature_extractor.data import linguistic_feature
+
         dir(linguistic_feature)
 
-        from acoustic_feature_extractor.data import midi_feature
-        dir(midi_feature)
-
         from acoustic_feature_extractor.data import phoneme
+
         dir(phoneme)
 
         from acoustic_feature_extractor.data import sampling_data
+
         dir(sampling_data)
 
         from acoustic_feature_extractor.data import spectrogram
+
         dir(spectrogram)
 
         from acoustic_feature_extractor.data import wave
+
         dir(wave)
 
     def test_import_utility(self):
         from acoustic_feature_extractor.utility import json_utility
+
         dir(json_utility)
